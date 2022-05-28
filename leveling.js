82f51d735fabdd980b90f57a51b4d9e3eedb32b4
EmmanuelSanchg <emmanuelsanchgdev@gmail.com>
2022-05-27 20:09:34 -0700

    decicion y subida de nivel funciona!!

--- leveling.js
+++ leveling.js
@@ -1,14 +1,6 @@
 let _cazador = "Debil - Mago - Espadachin - Tanque - Asesino"
-let estadisticas = {
-    vida: 0,
-    fuerza: 0,
-    habilidades: [],
-    addEstadisticas: function(v,f,h){
-        this.vida = v
-        this.fuerza = f
-        this.habilidades = h
-    }
-}
+let suerte = 1
+let level = 0
 
 
 let $name = "sun jin-woo"
@@ -28,6 +20,7 @@ let play = function(){
             case "debil":
                 $tipo = "Debil"
                 $skills = {
+                    dinero: 100,
                     vida: 200,
                     escudo: 100,
                     habilidades: {
@@ -40,6 +33,7 @@ let play = function(){
             case "mago":
                 $tipo = "Mago"
                 $skills = {
+                    dinero: 150,
                     vida: 250,
                     escudo: 150,
                     habilidades: {
@@ -66,18 +60,43 @@ let play = function(){
     }
     elegirTipo()
     
+    let probarSuerte = function(min, max){
+        return Math.floor( ( Math.random() * (max - min + 1) ) + min )
+    }
+
     let ingresarMasmorra = function(){
         let resultSuerte = ["jugador se encontro con un Mounstro",
                         "jugador no encontro ningun Mounstro",
                         "jugador se encontro con un Mounstro"]
-        let suerte = 1
 
-        let probarSuerte = function(min, max){
-            return Math.floor( ( Math.random() * (max - min + 1) ) + min )
-        }
         suerte = probarSuerte(0,2)
         console.log(resultSuerte[suerte])
+        
+        let batalla = function(){
+            let mounstruo = [
+                {tipo: "Gobling", ataqueBasico: 5, ataquePotenciado: 15},
+                {tipo: "Alto orco", ataqueBasico: 20, ataquePotenciado: 35},
+                {tipo: "Mounstruo desconocido", ataqueBasico: 30, ataquePotenciado: 45}
+            ]
+            probarSuerte(0,3)
+
+        }
+
+        if(suerte === 0 || suerte === 2){
+            console.log("la Batalla comienza")
+            // batalla()
+        } else {
+            let decicion = prompt("deseas seguir buscando Mounstruos?[y/n]").toLowerCase()
+            if(decicion === "y" || decicion === "si"){
+                console.log(level)
+                level++
+                console.log(level)
+                ingresarMasmorra()
+            }
+        }
+        console.log(level)
     }
+    ingresarMasmorra()
 
 }
 
