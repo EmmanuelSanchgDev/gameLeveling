29925e87dfa2bfa5b6e0ecd125ff2539eef08a40
EmmanuelSanchg <emmanuelsanchgdev@gmail.com>
2022-05-27 19:26:24 -0700

    se creo suerte para encontrar mounstruos, Fuenciona!!!

--- leveling.js
+++ leveling.js
@@ -16,39 +16,69 @@ let $tipo = ""
 let $skills = {}
 
 let play = function(){
-    let verificarName = String(prompt(`tu nombre es ${$name}? [y/n]`)).toLowerCase
+    let verificarName = String(prompt(`tu nombre es ${$name}? [y/n]`)).toLowerCase()
     if(verificarName === "n" || verificarName === "no"){
         $name = String(prompt("Entonces cual e stu nombre?"))
 
     }
     
-    $tipo = String(prompt(`Elige tu tipo de cazador...\n${_cazador}`)).toLowerCase
-    switch($tipo){
-        case "debil":
-            $skills = estadisticas.addEstadisticas(
-                200,
-                20,
-                ["golpe", "patada", "cabezado"]
-                )
-            break;
-        case "mago":
-            console.log("si")
-            break;
-        case "espadachin":
-            console.log("si")
-            break;
-        case "tanque":
-            console.log("si")
-            break;
-        case "asesino":
-            console.log("si")
-            break;
-        default:
-            console.log("por favor elige una de las opciones")
-            break;
+    let elegirTipo = function(){
+        $tipo = String(prompt(`Elige tu tipo de cazador...\n${_cazador}`)).toLowerCase()
+        switch($tipo){
+            case "debil":
+                $tipo = "Debil"
+                $skills = {
+                    vida: 200,
+                    escudo: 100,
+                    habilidades: {
+                        golpe: 20,
+                        patada: 30,
+                        cabezazo: 40
+                    }
+                }
+                break;
+            case "mago":
+                $tipo = "Mago"
+                $skills = {
+                    vida: 250,
+                    escudo: 150,
+                    habilidades: {
+                        flechaDeFuego: 55,
+                        curacion: 20,
+                        hechisoGlacear: 55
+                    }
+                }
+            //     break;
+            // case "espadachin":
+            //     console.log("si")
+            //     break;
+            // case "tanque":
+            //     console.log("si")
+            //     break;
+            // case "asesino":
+            //     console.log("si")
+            //     break;
+            default:
+                console.log("por favor elige una de las opciones")
+                elegirTipo()
+                break;
+        }
     }
+    elegirTipo()
     
+    let ingresarMasmorra = function(){
+        let resultSuerte = ["jugador se encontro con un Mounstro",
+                        "jugador no encontro ningun Mounstro",
+                        "jugador se encontro con un Mounstro"]
+        let suerte = 1
+
+        let probarSuerte = function(min, max){
+            return Math.floor( ( Math.random() * (max - min + 1) ) + min )
+        }
+        suerte = probarSuerte(0,2)
+        console.log(resultSuerte[suerte])
+    }
 
 }
 
-play()
+// play()
